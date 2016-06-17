99133ca60520316a7330e21beed74f50ee0eaae6
kahnar <youennlaborde@live.fr>
2016-06-17 23:16:50 +0200

    brute and shanks

---
+++ log/log_test.go
@@ -0,0 +1,44 @@
+// Created on 17 June 2016
+// author: youenn.kahlys.laborde
+
+package log
+
+import(
+    "testing"
+    "math/big"
+    "fmt"
+)
+
+func TestBruteForce(t *testing.T){
+    g := big.NewInt(2)
+    h := big.NewInt(5)
+    p := big.NewInt(1019)
+
+    x := BruteForce(g, h, p)
+    if x == nil {
+        t.Fatal("BruteForce failed")
+    }
+
+    r := g.Exp(g,x,p)
+    if r.Cmp(h) != 0 {
+        t.Error("BruteForce finds", r, "wants", h)
+    }
+}
+
+func TestShanks(t *testing.T){
+    g := big.NewInt(2)
+    h := big.NewInt(5)
+    p := big.NewInt(1019)
+
+    x := Shanks(g, h, p)
+    if x == nil {
+        t.Fatal("Shanks failed")
+    }
+
+    r := g.Exp(g,x,p)
+    if r.Cmp(h) != 0 {
+        t.Error("BruteForce finds", r, "wants", h)
+    }
+
+    fmt.Println("Resultat", x)
+}
